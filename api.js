8973f9c3a4856e0a4bd4c3781248bcce0451fc4e
Juribiyan <smugleaf@ya.ru>
2020-08-25 19:40:45 +0500

    Multiple comment sources, instant-0chan support

--- api.js
+++ api.js
@@ -1,34 +1,74 @@
-const thread = new URLSearchParams(document.location.search).get('thread') || '9901'
+const thrs = new URLSearchParams(document.location.search).get('threads')
+const threads = thrs ? thrs.split(',') : ['pl/10607']
 const srv = 'http://127.0.0.1:3000'
-var lastID = 0
 
-function getPosts(thread, after=null) {
-	return new Promise((resolve, reject) => {
-		fetch(`${srv}/${thread}${after ? `/${after}` : ''}`)
-		.then((response) => response.json())
-		.then(data => resolve(data.posts))
-	})
+const chans = {
+  pl: { engine: 'govnullch' },
+  cc: { engine: 'instant' },
+  metator: { engine: 'instant' }
 }
 
-function fetchPosts(after=lastID) {
-	getPosts(thread, after).then(posts => {
-		if (!posts.length) return;
+const engines = {
+	govnullch: {
+		getPosts: function(thread, after=null) {
+			return new Promise((resolve, reject) => {
+					fetch(`${srv}/${thread}${after ? `/${after}` : ''}`)
+					.then((response) => response.json())
+					.then(data => resolve(data.posts))
+				})
+		},
+		buildPost: function(html, chan) {
+			document.body.insertAdjacentHTML('beforeend', `<div class="post" data-chan="${chan}">${html}</div>`)
+		}
+	},
+	instant: {
+		getPosts: function(thread, after=null) {
+			return new Promise((resolve, reject) => {
+					fetch(`${srv}/${thread}${after ? `/${after}` : ''}`)
+					.then((response) => response.json())
+					.then(data => resolve(data))
+				})
+		},
+		buildPost: function(html, chan) {
+			document.body.insertAdjacentHTML('beforeend', `<div class="post" data-chan="${chan}">${html}</div>`)
+		}
+	},
+
+}
+
+function fetchPosts(thr) {
+	let lastID = lastPosts[thr.index]
+	engines[thr.engine].getPosts(thr.thread, lastID).then(posts => {
+		console.log(posts)
+		if (!posts || !posts.length) return;
 		posts.forEach(post => {
 			if (post.messageHtml != '') {
-				buildPost(post.messageHtml)
+				engines[thr.engine].buildPost(post.messageHtml, thr.chan)
 				let id = +post.id
 				if (id > lastID)
-					lastID = id
+					lastPosts[thr.index] = id
 			}
 		})
 	})
-	.finally(() => setTimeout(fetchPosts, 2000))
+	.finally(() => setTimeout(()=>fetchPosts(thr), 2000))
 }
 
-function buildPost(html) {
-	document.body.insertAdjacentHTML('beforeend', `<div class="post">${html}</div>`)
-}
+
+var lastPosts = []
 
 function main() {
-	fetchPosts()
+	let supported = []
+	threads.forEach((thr, index) => {
+		let chan = thr.split('/')[0]
+		if (chans.hasOwnProperty(chan) && chans[chan].engine && engines.hasOwnProperty(chans[chan].engine)) {
+			supported.push({
+				engine: chans[chan].engine,
+				chan: chan,
+				thread: thr,
+				index: index
+			})
+			lastPosts.push(0)
+		}
+	})
+	supported.forEach(fetchPosts)
 }
--- node-api/index.js
+++ node-api/index.js
@@ -2,24 +2,79 @@ const fetch = require('node-fetch')
 const express = require('express')
 const cors = require('cors')
 const app = express()
+const parse = require('node-html-parser').parse
+const https = require('https')
+const SocksProxyAgent = require('socks-proxy-agent')
 
-const api = `https://www.0chan.club/api`
-const session = 'FILLME' // ← DO IT
-
-const fetchOptions = {
-  headers: {
-    cookie: "disclaimer=1; welcome_text=true"
+const chans = {
+  pl: {
+    engine: 'govnullch',
+    api: `https://www.0chan.club/api`,
+    session: 'gx8ugpq6c3h0wiicvhcz75jf7wdap2c7',
+    fetchOptions: {
+      headers: {
+        cookie: "disclaimer=1; welcome_text=true"
+      }
+    }
+  },
+  cc: {
+    engine: 'instant',
+    api: 'https://0chan.cc'
+  },
+  metator: {
+    engine: 'instant',
+    api: 'http://metatorrkdagnx2njwvnzqeclsk3qbwabr6hori4vmivj25qy6s6gsad.onion',
+    fetchOptions: {
+      agent: new SocksProxyAgent('socks://127.0.0.1:9050')
+    }
   }
 }
 
 app.use(cors())
 
-app.get('/:thread/:after?', function (req, res) {
-  fetch(`${api}/thread?thread=${req.params.thread}${req.params.after ? `&after=${req.params.after}` : ''}&session=${session}`, fetchOptions)
+const engines = {
+  govnullch: function(chan, req, res) {
+    fetch(`${chan.api}/thread?thread=${req.params.thread}${req.params.after ? `&after=${req.params.after}` : ''}&session=${chan.session}`, chan.fetchOptions)
     .then(r => r.json())
     .then(json => {
       res.json(json)
     })
+    .catch(err => {
+      res.json(null)
+    })
+  },
+  instant: function(chan, req, res) {
+    let ts = req.params.thread.split("-")
+    , last = req.params.after ? +req.params.after : 0
+    , posts = []
+    fetch(`${chan.api}/${ts[0]}/res/${ts[1]}.html`, chan.fetchOptions)
+    .then(r => r.text())
+    .then(text => {
+      parse(text).querySelectorAll('.postnode').forEach(post => {
+        let id = +(post.getAttribute('data-id'))
+        if (id > last) {
+          posts.push({
+            id: id,
+            messageHtml: post.querySelector('.postmessage').innerHTML
+          })
+        }
+      })
+      res.json(posts)
+    })
+    .catch(err => {
+      console.warn(err)
+      res.json(null)
+    })
+  }
+}
+
+app.get('/:chan/:thread/:after?', function (req, res) {
+  if (chans.hasOwnProperty(req.params.chan) && engines.hasOwnProperty(chans[req.params.chan].engine)) {
+    engines[chans[req.params.chan].engine](chans[req.params.chan], req, res)
+  }
+  else {
+    res.json(null)
+  }
 })
  
 app.listen(3000)
